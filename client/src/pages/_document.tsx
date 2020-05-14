f72566c73ca074d16d053b8d5428e1f134ddc082
류일한 <RyuIL>
2020-05-14 23:58:17 +0900

    client: update boilerplate

---
+++ client/src/pages/_document.tsx
@@ -0,0 +1,1 @@
+export { default } from '~~/index/pages/_document.tsx'
---
+++ client/webpack.config.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const path = require("path");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const WebpackbarPlugin = require("webpackbar");
+const {CheckerPlugin} = require("awesome-typescript-loader");
+
+const files = fs.readdirSync(path.resolve(__dirname, "./serverless"));
+
+const entry = {};
+
+for (const file of files) {
+  entry[file.split(".")[0]] = path.resolve(__dirname, "./serverless", file);
+  console.log(file);
+}
+
+module.exports = {
+  entry,
+  mode: "production",
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+    ],
+  },
+  optimization: {
+    minimize: false,
+  },
+  output: {
+    filename: `[name].js`,
+    libraryTarget: "commonjs",
+    path: path.resolve(__dirname, "./dist/serverless/bundles"),
+  },
+  plugins: [
+    new WebpackbarPlugin({
+      color: "#fa5252",
+      name: "Serverless (Production)",
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new CheckerPlugin(),
+  ],
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  
+  stats: "errors-only",
+  target: "node",
+};
