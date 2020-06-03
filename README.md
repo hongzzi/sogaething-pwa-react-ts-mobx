f3d49d9d09e2508cc28a632109aa38c1fa8d753c
hongzzi <hongzzi.dev@gmail.com>
2020-06-03 10:39:54 +0900

    리드미 작성

--- README.md
+++ README.md
@@ -4,8 +4,6 @@
 [![npm version](https://img.shields.io/npm/v/react.svg?style=flat)](https://www.npmjs.com/package/react)
 > 매칭시스템을 도입한 중고거래 PWA
 
-![](./src/img/소개띵목업.png)
-
  - 목표기능
 1. 자체 알고리즘으로 원하는 물건의 판매글과 빠른 매칭서비스를 제공
 2. 광고없는 클린한 중고거래 게시판을 만들기
@@ -16,7 +14,7 @@
 
 <br>
 
-## :gear: 개발 환경 설정
+## :gear: Environment setting
 
 배포
 ```
@@ -32,7 +30,7 @@
 
 <br>
 
-## :hammer_and_pick: 개발 스택
+## :hammer_and_pick: Develop Stack
 
 **Front**
 
@@ -55,7 +53,7 @@
 - 환경 Windows 10 / eclipse (STS4)
 
 
-## :busts_in_silhouette: 정보
+## :busts_in_silhouette: Authors
 
 ### SSAFY 2기 Seoul 4반 심화프로젝트 <br>
 팀명 : 404 Not Found <br>
@@ -64,6 +62,6 @@
 
 <br>
 
-## 라이센스
+## License
 
  @copyright Samsung Software Academy
--- client/src/services/index/components/MatchForm/index.tsx
+++ client/src/services/index/components/MatchForm/index.tsx
@@ -74,15 +74,15 @@ export default (props: IMatchFormProps) => {
                     </Select>
                 </InputContainer>
                 <InputContainer>
-                    <Input type={'number'} value={match.minPrice} placeholder={'최소금액'} min={0} onChange={handleChangeMinPrice} />
+                    <Input type={'number'} value={match.minPrice} placeholder={'최소금액'} onChange={handleChangeMinPrice} />
                     <Span>~</Span>
-                    <Input type={'number'} value={match.maxPrice} placeholder={'최대금액'} min={0} onChange={handleChangeMaxPrice} />
+                    <Input type={'number'} value={match.maxPrice} placeholder={'최대금액'} onChange={handleChangeMaxPrice} />
                 </InputContainer>
                 <InputContainer>
                     <Select value={match.transaction} onChange={handleChangeTransaction}> <Option value={''}> 거래방법 </Option> <Option>직거래</Option> <Option>택배거래</Option></Select>
                 </InputContainer>
                 <InputContainer onClick={handleClickHashtag}>
-                    <HashTag>해시태그 {matchStore.hashtag.length}
+                    <HashTag>해시태그 <HashTagContainer>{match.hashtag.length} 개</HashTagContainer>
                     </HashTag>
                 </InputContainer>
             </FormContainer>
@@ -146,11 +146,10 @@ const Span = styled.span`
 `
 
 const Select = styled.select`
-    background: url(${DropdownIcon}) no-repeat 95% 50%;
     -webkit-appearance: none;
        -moz-appearance: none;
             appearance: none;
-    background: transparent;
+    background: url(${DropdownIcon}) no-repeat 95% 50%;
     width: 100%;
     height: 2rem;
     font-size: 15px;
@@ -173,16 +172,19 @@ const Option = styled.option`
 
 const HashTag = styled.div`
     background: url(${ExpandIcon}) no-repeat 95% 50%;
-    display: flex;
-    justify-content: start;
-    align-items: center;
     width: 100%;
-    height: 2rem;
+    height: auto;
     font-size: 15px;
     color: #929292;
     font-weight: bold;
     border: solid 0;
-    padding: 0.5rem 0.1rem;
+    padding: 0.5rem 0.2rem;
+`
+
+const HashTagContainer = styled.div`
+    display: block;
+    float: right;
+    padding: 0 5rem;
 `
 
 const BtnContainer = styled.div``
