43f51b0b0b0d0d46729310d55211065848f1cde2
문지현 <puppy613@hanmail.net>
2020-06-05 12:35:31 +0900

    S02P31A404-107 Add autocomplete-desc

--- server/market/src/main/java/com/ssafy/market/domain/hashtag/repository/HashtagRepository.java
+++ server/market/src/main/java/com/ssafy/market/domain/hashtag/repository/HashtagRepository.java
@@ -15,6 +15,9 @@ public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
     List<Hashtag> findByProduct(Product product);
     long countByHashtag(String Hashtag);
 
+    @Query(value = "select hashtag, count(hashtag) count from hashtag where hashtag like :hashtag% group by hashtag order by count(hashtag) desc;", nativeQuery = true)
+    List<HashtagCountOutput> countAllByHashtag(@Param("hashtag") String hashtag);
+
     @Query(value = "SELECT DISTINCT hashtag FROM hashtag WHERE hashtag LIKE :hashtag%", nativeQuery = true)
     List<String> findAutocompleteHashtag(@Param("hashtag") String hashtag);
 
